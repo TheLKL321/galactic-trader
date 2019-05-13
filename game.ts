b98647f420e7b9a10900d4df6f884fa5f4c87aac
Łukasz Zarębski <lkl.zarebski@gmail.com>
2019-05-13 12:38:06 +0200

    add countdown timer

--- game.ts
+++ game.ts
@@ -1,1 +1,19 @@
 (<HTMLElement> document.querySelector("#gameStateBar > .text-left")).innerText = sessionStorage.getItem("nickname");
+startCountdown(300);
+let money = 1984;
+let moneyText = (<HTMLElement> document.querySelector("#gameStateBar > .text-right"));
+moneyText.innerText = String(money);
+
+function startCountdown(seconds){
+    let counter = seconds;
+    let timerText = (<HTMLElement> document.querySelector("#gameStateBar > .text-middle"));
+
+    const interval = setInterval(() => {
+        timerText.innerText = counter;
+        counter--;
+
+        if (counter < 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
